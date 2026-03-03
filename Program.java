d28dece5aca9564ea205212fbc681cd0c4fb28ba
Localxxv <localgxv@gmail.com>
2026-03-03 21:33:31 +0100

    Egzamin 2024

--- Program.java
+++ Program.java
@@ -1,10 +1,58 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        CalculatedFunction coeff= new Polynomial(-1,-8,2,1);
-        CalculatedFunction f = new AnyFunction(x ->Math.sin(x)*2);
 
-        Integral intglr1 = new TrapezoidMethod(coeff, 0, 4, 16);
-        System.out.println(intglr1.calculate());
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.println("od barku do lokcia: ");
+        double elbowShoulder = sc.nextDouble();
+
+        System.out.println("od lokcia do nadgarstka: ");
+        double wristElbow = sc.nextDouble();
+
+        System.out.println("od barku do nadgarstka: ");
+        double shoulderWrist = sc.nextDouble();
+
+        System.out.println("od biodra do kolana: ");
+        double kneeHip = sc.nextDouble();
+
+        System.out.println("od biodra do biorka: ");
+        double hipShoulder = sc.nextDouble();
+
+        System.out.println("od barku do kolana: ");
+        double shoulderKnee = sc.nextDouble();
+
+
+        System.out.println("od kolana do kostki: ");
+        double ankleKnee = sc.nextDouble();
+
+        System.out.println("od kolana do biodra: ");
+        double kneeHip2 = sc.nextDouble();
+
+        System.out.println("od biodra do kostki: ");
+        double hipAnkle = sc.nextDouble();
+
+
+
+
+
+        Angles angles = new Angles(
+                wristElbow, elbowShoulder, shoulderWrist,
+                kneeHip, hipShoulder, shoulderKnee,
+                ankleKnee, kneeHip2, hipAnkle
+        );
+
+        System.out.println("Kąt w lokciu");
+        System.out.println(angles.getElbowAngleDeg());
+
+        System.out.println("Kąt w biodrze");
+        System.out.println(angles.getHipAngleDeg());
+
+        System.out.println("Kąt w kolanie");
+        System.out.println(angles.getKneeAngleDeg());
 
 
     }
